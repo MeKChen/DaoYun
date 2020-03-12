9cbc306b39110955df6094d0b930af81d4054fae
Fei <1257471371@qq.com>
2020-03-12 21:48:00 +0800

    构建脚手架及构建项目，配置全局样式、路由、elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        到云
+        <el-button type="primary">登录</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'login'
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
